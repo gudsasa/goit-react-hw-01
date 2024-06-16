684b13b9c1c4cee186d807036aa68e69550a4faf
gudsasa <gudsasa@gmail.com>
2024-06-16 11:46:27 +0300

    goit-react-hw-01

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,9 +1,15 @@
+import css from './FrindListItem.modulr.css';
+
 const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <div>
       <img src={avatar} alt="Friend" width="48" />
       <p>{name}</p>
-      <p>{isOnline}</p>
+      {isOnline ? (
+        <p className={css.online}>Online</p>
+      ) : (
+        <p className={css.offline}>Offline</p>
+      )}
     </div>
   );
 };
